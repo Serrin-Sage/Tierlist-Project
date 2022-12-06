88675fede1d9c1a4e42d9139c50dd1a94421b5ae
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2022-12-06 07:53:30 -0500

    added README, adjusted jsx for sending characters to tiers (must be a more efficient way)

---
+++ README.md
@@ -0,0 +1,13 @@
+# TierList Creator
+## Functionality:
+    
+    A user can sign in if they exist in the database. If not they can sign up which will add them to the database.
+
+    After sign in the user can view default characters and add them to the tier of their choosing. Characters can be removed from a tier and placed in new tiers. Characters can only exist in one tier. The user can add new characters if they provide a name, thumbnail image, display image, strength and weakness for the character. 
+
+    Tier list can be saved globally and viewed by each user in the Global Tiers page. Each user can Agree or Disagree on each Global Tier list.
+
+
+new dependencies used:
+- react-toastify (creates user notifications)
+- html2canvas (creates images of divs)
--- src/components/CharacterDisplay.jsx
+++ src/components/CharacterDisplay.jsx
@@ -1,19 +1,43 @@
-const CharacterDisplay = ({ displayChar, userTiers,setUserTiers, setSTier, setATier }) => {
+const CharacterDisplay = ({ displayChar, userTiers, setUserTiers, setSTier, setATier }) => {
 
-    const assignToTier = (clickedTier, tierSetter) => {
-        //NEED TO FIND A WAY TO NOT ASSIGN TO MULTIPLE TIERS
-        setUserTiers((prevState) => {
-            if (prevState.includes(clickedTier)) return [...prevState]
-            return [...prevState, clickedTier]
+    // const assignToTier = (clickedTier, tierSetter) => {
+    //     //NEED TO FIND A WAY TO NOT ASSIGN TO MULTIPLE TIERS
+    //     setUserTiers((prevState) => {
+    //         if (prevState.includes(clickedTier)) return [...prevState]
+    //         return [...prevState, clickedTier]
+    //     })
+
+    //     tierSetter((prevState) => {
+    //         if (prevState.includes(clickedTier)) return [...prevState]
+    //         return [...prevState, clickedTier]
+    //     })
+    // }
+
+    const assignToS = ( character ) => {
+        setSTier((prevState) => {
+            if (prevState.includes(character)) return [...prevState]
+            return [...prevState, character]
         })
-
-        tierSetter((prevState) => {
-            if (prevState.includes(clickedTier)) return [...prevState]
-            return [...prevState, clickedTier]
+        setATier((prevState) => {
+            return [...prevState.filter((toRemove) =>{
+                return toRemove.id !== character.id
+            })]
         })
-        
-
     }
+    
+    const assignToA = ( character ) => {
+        setATier((prevState) => {
+            if (prevState.includes(character)) return [...prevState]
+            return [...prevState, character]
+        })
+        setSTier((prevState) => {
+            return [...prevState.filter((toRemove) =>{
+                return toRemove.id !== character.id
+            })]
+        })
+    }
+
+    
     return (
         <div className="display-container">
             {
@@ -23,8 +47,8 @@ const CharacterDisplay = ({ displayChar, userTiers,setUserTiers, setSTier, setAT
                     <p>Strength: {displayChar.strength}</p>
                     <p>Weakness: {displayChar.weakness}</p>
                     <div className="tier-btn-container">
-                        <div className="tier-btn" onClick={() => assignToTier(displayChar, setSTier)}>S</div>
-                        <div className="tier-btn" onClick={() => assignToTier(displayChar, setATier)}>A</div>
+                        <div className="tier-btn" onClick={() => assignToS(displayChar)}>S</div>
+                        <div className="tier-btn" onClick={() => assignToA(displayChar)}>A</div>
                         <div className="tier-btn">B</div>
                         <div className="tier-btn">C</div>
                         <div className="tier-btn">F</div>
