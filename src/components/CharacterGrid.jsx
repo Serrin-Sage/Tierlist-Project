9bd4ef05ffbde9f0d5d3c87339cbe35541432a26
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2022-12-07 22:46:04 -0500

    New character grid select works as intended, more css styling

--- src/components/CharacterGrid.jsx
+++ src/components/CharacterGrid.jsx
@@ -1,38 +1,33 @@
 import { useState } from 'react'
 import Form from './Form'
+import CharacterCard from './CharacterCard'
 
 const CharacterGrid = ({ allCharacters, setAllCharacters, setDisplayChar }) => {
     
-    const [clicked, setClicked] = useState(false)
+    
     const [showForm, setShowForm] = useState(false)
+    const [selected, setSelected] = useState(false)
 
-    const hoverCharacter = (clickedDiv) => {
-        if (clicked) return
-        setDisplayChar(clickedDiv)
-    }
-
-    const selectCharacter = (clickedDiv, event) => {
-        event.target.style.border = "solid 2px red"
-        setClicked(true)
-        setDisplayChar(clickedDiv)
-        setTimeout(() => {
-            setClicked(false)
-            event.target.style.border = "solid 2px transparent"
-        }, 2000)
+    const hoverCharacter = (hoverDiv) => {
+        if (selected) return
+        setDisplayChar(hoverDiv)
     }
 
     const displayForm = () => {
         setShowForm(current => !current)
     }
+
     return (
         <div className="character-grid-container">
             <div className="character-grid">
                 {
                     allCharacters.map((character) => {
                         return (
-                            <div key={character.id} className="grid-card" onMouseOver={() => hoverCharacter(character)} onClick={(e) => selectCharacter(character, e)}>
-                                <img src={character.thumbnail} alt="" />
-                            </div>
+                            <CharacterCard 
+                                character={character} 
+                                setDisplayChar={setDisplayChar} 
+                                hoverCharacter={hoverCharacter}
+                                setSelected={setSelected}/>
                         )
                     })
                 }
