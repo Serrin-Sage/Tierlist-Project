3c270aff1000042a5d9eb6fa93b467781213d9d0
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2022-12-08 14:06:38 -0500

    added user characters, each user will be able to have unique characters to choose from

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -8,6 +8,7 @@ import TierList from './TierList'
 
 const MainPage = ({ setLoggedIn, setNewUser, currentUser, setSavedTierList }) => {
 
+    
     let placeHolder = {
         name: "Choose a Character",
         thumbnail: "",
@@ -16,7 +17,7 @@ const MainPage = ({ setLoggedIn, setNewUser, currentUser, setSavedTierList }) =>
         weakness: "N/A"
     }
 
-    const [allCharacters, setAllCharacters] = useState([])
+    const [allCharacters, setAllCharacters] = useState(currentUser.characters)
     const [displayChar, setDisplayChar] = useState(placeHolder)
     const [sTier, setSTier] = useState([])
     const [aTier, setATier] = useState([])
@@ -25,13 +26,14 @@ const MainPage = ({ setLoggedIn, setNewUser, currentUser, setSavedTierList }) =>
     const [fTier, setFTier] = useState([])
 
 
-    useEffect(() => {
-        fetch("http://localhost:3000/characters")
-        .then((res) => res.json())
-        .then((data) => {
-            setAllCharacters(data)
-        })
-    },[setAllCharacters])
+    // useEffect(() => {
+    //     fetch("http://localhost:3000/users")
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         console.log(data.characters)
+    //         setAllCharacters(data)
+    //     })
+    // },[setAllCharacters])
 
     
     return (
